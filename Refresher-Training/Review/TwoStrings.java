6e385ca1c18d2d7a05ccff4f823877d8a7c351e0
Vikash23151 <vikash.gla_cs22@gla.ac.in>
2026-08-05 17:03:05 +0530

    [ Vikash ] Add: added array & string based review, hackerrank practice problems

---
+++ Refresher-Training/Review/TwoStrings.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class TwoStrings {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();
+        sc.nextLine();
+        while (q-- > 0) {
+            String s1 = sc.nextLine();
+            String s2 = sc.nextLine();
+            System.out.println(twoStrings(s1, s2));
+        }
+        sc.close();
+    }
+
+    public static String twoStrings(String s1, String s2) {
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+        boolean[] arr = new boolean[26];
+        for (char c : s1.toCharArray()) {
+            arr[c - 'a'] = true;
+        }
+        for (char c : s2.toCharArray()) {
+            if (arr[c - 'a']) {
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+}
